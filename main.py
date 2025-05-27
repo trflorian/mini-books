470ceb553814e3533e6a9ba1fbc2591c3eb44b3b
trflorian <florian.trautweiler@hispeed.ch>
2025-05-27 22:30:03 +0200

    cleanup code

--- main.py
+++ main.py
@@ -6,24 +6,34 @@ from docx.shared import Mm
 
 from utils import CellDirection, set_vertical_cell_direction
 
+# A4 Portrait Page Setup, all values in millimeters
+PAGE_ORIENTATION = WD_ORIENT.PORTRAIT
+PAGE_WIDTH = 210
+PAGE_HEIGHT = 297
+MARGIN = 4.0
+
 document = Document()
+page_section = document.sections[0]
 
-section = document.sections[0]
-section.orientation = WD_ORIENT.PORTRAIT
-section.page_width = Mm(210)
-section.page_height = Mm(297)
-section.left_margin = Mm(4.0)
-section.right_margin = Mm(4.0)
-section.top_margin = Mm(4.0)
-section.bottom_margin = Mm(0.0)
+# Page Setup
+page_section.orientation = PAGE_ORIENTATION
+page_section.page_width = Mm(PAGE_WIDTH)
+page_section.page_height = Mm(PAGE_HEIGHT)
 
+# Margins
+page_section.left_margin = Mm(MARGIN)
+page_section.right_margin = Mm(MARGIN)
+page_section.top_margin = Mm(MARGIN)
+page_section.bottom_margin = Mm(MARGIN)
+
+# Table Setup
 table = document.add_table(rows=4, cols=2)
 
 cell_indices = [[2, 1], [3, 8], [4, 7], [5, 6]]
 cell_orientations = 4 * [[CellDirection.TB_RL, CellDirection.BT_LR]]
 
 for row_id, row in enumerate(table.rows):
-    row.height = Mm(72)
+    row.height = Mm(PAGE_HEIGHT / len(table.rows) - MARGIN)
     for col_id, cell in enumerate(row.cells):
         cell_id = cell_indices[row_id][col_id]
         cell_orientation = cell_orientations[row_id][col_id]
@@ -36,7 +46,5 @@ for row_id, row in enumerate(table.rows):
         set_vertical_cell_direction(cell, cell_orientation)
 
         cell.paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-
-document.add_page_break()
 
 document.save("demo.docx")
